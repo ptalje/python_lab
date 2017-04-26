3884a961d950cd321757e2069fc412ffe5cafc10
ptalje <per.taljemark@thirdbase.se>
2017-04-26 14:52:31 +0200

    Update guess_number.py
    Python3 compatible

--- guess_number.py
+++ guess_number.py
@@ -6,11 +6,11 @@ random_number = randint(1, 10)
 guesses_left = 3
 # Start your game!
 while guesses_left != 0:
-    guess = int(raw_input("Guess the number"))
+    guess = int(input("Guess the number"))
     if guess == random_number:
-        print "You win!"
+        print("You win!")
         break
     guesses_left -= 1
 else:
-    print "You lose."
-    print "I thought of " + str(random_number)
+    print("You lose.")
+    print("I thought of " + str(random_number))
